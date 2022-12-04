1e5feefc89285715fd14dd1b362d6aff3eb80cdd
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-12-04 15:48:25 -0800

    Create template.html file and add bootstrap CDN and content.

--- index.js
+++ index.js
@@ -36,8 +36,126 @@ inquirer
         const officeNumber = response.manager_number;
         const manager = new Manager(name, id, email, officeNumber);
         employees.push(manager);
+        employeeOptions();
     }
     );
 
-  // Create Engineer Employee Function
-  // Create Intern Employee Function
+// Create option function to determine whether another employee will be added.
+const createAnother = function () {
+    const choices = ["Yes, I'd like to create another Employee.", "No, I'd like to exit the application and see my results."];
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Would you like to add another employee?',
+                name: "options",
+                choices: choices,
+            }
+        ])
+        .then((response) => {
+            if (response.options === choices[0]) {
+                console.log("Lets create another");
+                employeeOptions();
+            } else { 
+                // TODO: Run function that will create the html and/or css files. Use employees array to loop through objects and create cards.
+                return console.log(employees) 
+            }
+        }
+        );
+};
+
+// Create option function to determine whether the next employee will be an Engineer or an Intern.
+const employeeOptions = function () {
+    const choices = ["Engineer", "Intern"];
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Which type of Employee would you like to add?',
+                name: "options",
+                choices: choices,
+            }
+        ])
+        .then((response) => {
+            if (response.options === choices[0]) {
+                createEngineer();
+            } else { createIntern(); }
+        }
+        );
+};
+
+// Create Engineer Employee Function
+const createEngineer = function () {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the engineer\'s name:',
+                name: 'engineer_name',
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s employee ID:',
+                name: 'engineer_id',
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s employee email address:',
+                name: 'engineer_email',
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s GitHub username',
+                name: 'engineer_github',
+            },
+        ])
+        .then((response) => {
+            const name = response.engineer_name;
+            const id = response.engineer_id;
+            const email = response.engineer_email;
+            const github = response.engineer_github;
+            const engineer = new Engineer(name, id, email, github);
+            employees.push(engineer);
+            console.log(employees);
+            createAnother();
+        }
+        );
+};
+
+// Create Intern Employee Function
+const createIntern = function () {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the team intern\'s name:',
+                name: 'intern_name',
+            },
+            {
+                type: 'input',
+                message: 'What is the team intern\'s employee ID:',
+                name: 'intern_id',
+            },
+            {
+                type: 'input',
+                message: 'What is the team intern\'s employee email address:',
+                name: 'intern_email',
+            },
+            {
+                type: 'input',
+                message: 'What is the team intern\'s school:',
+                name: 'intern_school',
+            },
+        ])
+        .then((response) => {
+            const name = response.intern_name;
+            const id = response.intern_id;
+            const email = response.intern_email;
+            const school = response.intern_school;
+            const intern = new Intern(name, id, email, school);
+            employees.push(intern);
+            console.log(employees);
+            createAnother();
+        }
+        );
+};
